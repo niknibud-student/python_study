2e4a432d94913d8cfc651baefb5a92f62898511f
Dubinkin Pavel <niknibud@gmail.com>
2020-06-21 13:06:25 +0300

    Программа Шифр Цезаря по идее Фоксфорда. Очень запутано и не очевидно

--- Foxford/lesson2/Zecar_crypt/main.py
+++ Foxford/lesson2/Zecar_crypt/main.py
@@ -1,14 +1,17 @@
 
 
 # Функция для шифровки
-def encrypt():
-
-    pass
+def encrypt(list_of_symbols_from_text):
+    list_of_codes_of_symbols = []
 
 
 # Функция для дешифровки
 def decrypt(text):
     pass
+
+
+
+
 
 
 print('Зашифровать(1) или расшифровать(2) сообщение? Введите 1 или 2:')
@@ -17,3 +20,5 @@ print('Введите текст:')
 text = input()
 length = len(text)
 list_of_symbols_from_text = list(text)
+
+[list_of_symbols_from_text.append(text[i]) for i in range(length)]
