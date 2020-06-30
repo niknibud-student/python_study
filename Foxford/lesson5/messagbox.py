083ac31b877a80743205b806dd3c63a0c9ece172
Dubinkin Pavel <niknibud@gmail.com>
2020-06-30 12:34:14 +0300

    Добавил файл messagebox.py, который показывает вид askyesno

---
+++ Foxford/lesson5/messagbox.py
@@ -0,0 +1,18 @@
+from tkinter import Tk, Entry, Label, Button
+from tkinter import messagebox as mbox
+
+def check():
+    answer = mbox.askyesno('Вопрос', 'Перенести данные?')
+
+    if answer:
+        s = entry.get()
+        entry.delete(0, END)
+        label['text'] = s
+
+root = Tk()
+entry = Entry()
+entry.pack(pady=10)
+Button(text='Передать', command=check).pack()
+label = Label(height=3)
+label.pack()
+root.mainloop()
