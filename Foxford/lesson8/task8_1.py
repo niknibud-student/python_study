89f514a7c192d1dda3ed7450ddc5d469641c4fdb
Dubinkin Pavel <niknibud@gmail.com>
2020-07-12 17:30:17 +0300

    Сдал задания урока 8

---
+++ Foxford/lesson8/task8_1.py
@@ -0,0 +1,12 @@
+from graph import *
+
+polygon([(100, 100) , (200, 50), (300, 100), (100, 100)])
+
+penColor('yellow')
+penSize(2)
+polygon([(100, 200) , (200, 150), (300, 200), (100, 200)])
+
+penColor('red')
+brushColor('red')
+polygon([(100, 300) , (200, 250), (300, 300), (100, 300)])
+run()
