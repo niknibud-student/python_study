91b44be7de9fcae23b2651b25eb9609bbfcc1a04
Dubinkin Pavel <niknibud@gmail.com>
2020-07-15 16:57:38 +0300

    Добавил пример вызова черепашки

---
+++ Foxford/lesson8/sample/sample1.py
@@ -0,0 +1,13 @@
+from turtle import TK, Turtle
+
+t = Turtle()
+t.screen.setup(800, 800)
+
+for i in range(20):
+    t.fd(8)
+    t.up()
+    t.fd(8)
+    t.down()
+
+t.screen.exitonclick()
+t.screen.mainloop()
